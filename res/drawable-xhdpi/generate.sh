63e7a9fe9ed3dd3ef2b0fca2fe1db6edc8385432
Mark Rudolph <mark.rudolph@sensorcon.com>
2013-08-07 18:51:54 -0400

    Fixed sizes. Better for small screen.

--- res/drawable-xhdpi/generate.sh
+++ res/drawable-xhdpi/generate.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 for FILE in \
-info.png
+face_bad.png \
+face_good.png \
+face_moderate.png \
+face_unknown.png \
+face_unknown_1.png \
+face_unknown_2.png \
+gas_view_bad.png \
+gas_view_good.png \
+gas_view_moderate.png
 do
-	convert $FILE -resize 35% ../drawable-ldpi/$FILE
-	convert $FILE -resize 40% ../drawable-mdpi/$FILE
-	convert $FILE -resize 70% ../drawable-hdpi/$FILE
-	convert $FILE -resize 145% ../drawable-xxhdpi/$FILE
+	convert $FILE -resize 25% ../drawable-ldpi/$FILE
+	convert $FILE -resize 50% ../drawable-mdpi/$FILE
+	convert $FILE -resize 75% ../drawable-hdpi/$FILE
+	convert $FILE -resize 150% ../drawable-xxhdpi/$FILE
 done
